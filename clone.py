606dd2f6c1efc7287506f6298cf327ebb233bc69
Chandler Tayek <chandlertayek@gmail.com>
2017-04-15 17:13:18 -0700

    Feat: data augmentation and left/right camera use

--- clone.py
+++ clone.py
@@ -13,25 +13,41 @@ with open('data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    image = cv2.imread(filename)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
-    
-X_train = np.array(images)
-y_train = np.array(measurements)
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        image = cv2.imread(filename)
+        images.append(image)
+        
+        # Add a correction value to the right and left camera images
+        measurement = float(line[3])
+        if i == 1:
+            measurement += 0.2
+        else if i == 2:
+            measurement -=  0.2
+        measurements.append(measurement)
 
+# Augment the data by fliping it
+augmented_images, augmented_measurements = [], []
+for image,measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image,1))
+    augmented_measurements.append(measurement*-1.0)
+    
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
+    
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
-from keras.layers.poolinng import MaxPooling2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 # Normalize pixels
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160,320,3)))
-
+model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Convolution2D(6,5,5, activation = "relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5, activation = "relu"))
@@ -39,7 +55,7 @@ model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
 model.add(Dense(84))
-model.add(DEnse(1))
+model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch = 3)

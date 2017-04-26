7b32130f584cb84bbc9b9b14e3213031344b2d4b
Chandler Tayek <chandlertayek@gmail.com>
2017-04-25 22:37:06 -0700

    feat: Use Udacity data set and rename clone

--- clone.py
+++ clone.py
@@ -12,20 +12,30 @@ with open('data/driving_log.csv') as csvfile:
         
 images = []
 measurements = []
+
+#Ignore the header
+#take out the first line in lines
+ignoreHeader = False
+temp = lines[1]
+temp1 = temp[0]
+
+root = r"E:\Udacity\self-driving-engineering\CarND-Behavioral-Cloning-P3\data\IMG"
 for line in lines:
-    for i in range(3):
-        source_path = line[i]
-        filename = source_path.split('/')[-1]
-        image = cv2.imread(filename)
-        images.append(image)
-        
-        # Add a correction value to the right and left camera images
-        measurement = float(line[3])
-        if i == 1:
-            measurement += 0.2
-        elif i == 2:
-            measurement -=  0.2
-        measurements.append(measurement)
+    if(ignoreHeader):
+        for i in range(3):
+            source_path = line[i]
+            filename = source_path.split('/')[-1]
+            image = cv2.imread(root +'/' +  filename)
+            images.append(image)
+            
+            # Add a correction value to the right and left camera images
+            measurement = float(line[3])
+            if i == 1:
+                measurement += 0.2
+            elif i == 2:
+                measurement -=  0.2
+            measurements.append(measurement)
+    ignoreHeader = True
 
 # Augment the data by fliping it
 augmented_images, augmented_measurements = [], []
@@ -46,18 +56,38 @@ from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 # Normalize pixels
+
+
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160,320,3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
-model.add(Convolution2D(6,5,5, activation = "relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5, activation = "relu"))
-model.add(MaxPooling2D())
+model.add(Convolution2D(24,5,5, subsample = (2,2), activation = "relu"))
+model.add(Convolution2D(36,5,5, subsample = (2,2), activation = "relu"))
+model.add(Convolution2D(48,5,5, subsample = (2,2), activation = "relu"))
+model.add(Convolution2D(64,3,3, activation = "relu"))
+model.add(Convolution2D(64,3,3, activation = "relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
-
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch = 3)
+print()
+print("Fitting the model")
+history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+print("model fitted")
+import matplotlib.pyplot as plt
 
+
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
 model.save('model.h5')
